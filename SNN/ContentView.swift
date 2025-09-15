0d89d69ab15c30c8ec1c245dd3ff67916b9ef3b0
Rob J Goedman <goedman@icloud.com>
2025-09-15 10:51:07 -0600

    Initial Commit

---
+++ SNN/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SNN
+//
+//  Created by Robert Goedman on 9/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SNN/SNNApp.swift
@@ -0,0 +1,17 @@
+//
+//  SNNApp.swift
+//  SNN
+//
+//  Created by Robert Goedman on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SNNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
